6c054751b9e75e0ff111edc78530cbd0c0c719ab
Tiếp Bước <hoangminhtuan09032002@gmail.com>
2026-03-10 20:37:36 +0700

    Update vangv.js

--- vangv.js
+++ vangv.js
@@ -1,4 +1,4 @@
- const REVERSED_API_KEY = "ADHvlJk9rfZ40q7ju_r-yVQl1ZqW4Z-MDySzAI";
+ const REVERSED_API_KEY = "AnUY_QYgjLMUu1bJjTFMvg11ROujf-QKDySazIA";
 const GEMINI_API_KEY = REVERSED_API_KEY.split('').reverse().join('');
 
 const firebaseConfig = {
@@ -2566,6 +2566,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 });
+
 
 
 
